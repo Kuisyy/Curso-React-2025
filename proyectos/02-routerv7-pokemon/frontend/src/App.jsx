dcba96b739a5169d79b725fd3424d7a2a600570c
[Kuisyy] <[luis3valderrubio@gmail.com]>
2025-01-24 12:27:43 +0100

    02 creado y poniendo routes

---
+++ proyectos/02-routerv7-pokemon/frontend/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className='text-orange-500 text-xl'>App</div>
+  )
+}
+
+export default App;
---
+++ proyectos/02-routerv7-pokemon/frontend/src/routes/paths.js
@@ -0,0 +1,7 @@
+export const ROUTES = {
+    HOME:'/',
+    SEARCH:'/search',
+    FAVOURITES:'/favourites',
+    POKEMON:'/pokemon/:name',
+    ABOUT:'/about'
+};
